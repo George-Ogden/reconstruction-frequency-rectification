22bd19d13aa5f679d8ba67ecec3e3c0ca871e8d1
George-Ogden <george-ogden@outlook.com>
2024-11-20 16:45:51 +0000

    test and evaluate wave sweep

---
+++ scripts/VQVAE/metrics/celeba_recon_wave_sweep.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -x
+
+VERSION='celeba_recon_wave_sweep'
+METRICS='psnr ssim lpips fid lfd'
+EPOCH='epoch_005_seed_1112_with_input'
+
+for W in 0.001 0.01 0.1 1 10 100; do
+    FDRF='./VQVAE/results/'$VERSION-$W'/'$EPOCH
+    LOGS='./VQVAE/results/'$VERSION-$W'/metrics_'$EPOCH'.txt'
+    python ./metrics/calc_metrics.py \
+        --metrics $METRICS \
+        --fdrf $FDRF \
+        --logs $LOGS
+done
---
+++ scripts/VQVAE/test/celeba_recon_wave_sweep.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -x
+
+VERSION='celeba_recon_wave_sweep'
+DATA='filelist'
+DATAROOT='./datasets/celeba'
+DATALIST='./datasets/celeba_recon_lists/test.txt'
+EXP_DIR='./VQVAE/experiments/'$VERSION
+RES_DIR='./VQVAE/results/'$VERSION
+WORKER=8
+
+for W in 0.001 0.01 0.1 1 10 100; do
+    python ./VQVAE/test.py \
+        --dataset $DATA \
+        --dataroot $DATAROOT \
+        --datalist $DATALIST \
+        --workers $WORKER \
+        --batchSize 1 \
+        --imageSize 224 \
+        --nz 128 \
+        --nblk 1 \
+        --expf $EXP_DIR-$W \
+        --manualSeed 1112 \
+        --epoch_test 5 \
+        --eval \
+        --resf $RES_DIR-$W \
+        --show_input 
+done
